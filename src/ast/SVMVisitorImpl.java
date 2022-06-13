04d664f772de1e7531ebb46122cbc84d661aac30
gaiaghi <gaia.ghid@gmail.com>
2022-06-13 12:29:09 +0200

    Aggiunta della classe Instruction

--- src/ast/SVMVisitorImpl.java
+++ src/ast/SVMVisitorImpl.java
@@ -1,15 +1,17 @@
 package ast;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import interpreter.ExecuteVM;
 import svm.SVMLexer;
 import svm.SVMParser;
 import svm.SVMBaseVisitor;
+import util.Instruction;
 
 public class SVMVisitorImpl extends SVMBaseVisitor<Void> {
-
-	public int[] code = new int[ExecuteVM.CODESIZE];    
+ 
+	public ArrayList<Instruction> code = new ArrayList<Instruction>();
     private int i = 0;
     private HashMap<String,Integer> labelAdd = new HashMap<String,Integer>(); 
     private HashMap<Integer,String> labelRef = new HashMap<Integer,String>();
@@ -18,7 +20,8 @@ public class SVMVisitorImpl extends SVMBaseVisitor<Void> {
     public Void visitAssembly(SVMParser.AssemblyContext ctx) { 
     	visitChildren(ctx);
     	for (Integer refAdd: labelRef.keySet()) {
-            code[refAdd]=labelAdd.get(labelRef.get(refAdd));
+    		//TODO
+//            code[refAdd]=labelAdd.get(labelRef.get(refAdd));
     	}
     	return null;
     }
@@ -27,155 +30,129 @@ public class SVMVisitorImpl extends SVMBaseVisitor<Void> {
     public Void visitInstruction(SVMParser.InstructionContext ctx) { 
     	switch (ctx.getStart().getType()) {
 			case SVMLexer.PUSH:		
-				code[i++] = SVMParser.PUSH; 
-	            code[i++] = Integer.parseInt(ctx.reg.getText());
+				code.add(new Instruction(SVMParser.PUSH, ctx.reg.getText()));
 				break;
+				
 			case SVMLexer.POP:
-				code[i++] = SVMParser.POP;
+				code.add(new Instruction(SVMParser.POP));
 				break;
+				
 			case SVMLexer.ADD:
-				code[i++] = SVMParser.ADD;
-				code[i++] = Integer.parseInt(ctx.res.getText());
-				code[i++] = Integer.parseInt(ctx.term1.getText());
-				code[i++] = Integer.parseInt(ctx.term2.getText());
+				code.add(new Instruction(SVMParser.ADD, ctx.res.getText(), ctx.term1.getText(), ctx.term2.getText()));
 				break;
+				
 			case SVMLexer.SUB:
-				code[i++] = SVMParser.SUB;
-				code[i++] = Integer.parseInt(ctx.res.getText());
-				code[i++] = Integer.parseInt(ctx.term1.getText());
-				code[i++] = Integer.parseInt(ctx.term2.getText());
+				code.add(new Instruction(SVMParser.SUB, ctx.res.getText(), ctx.term1.getText(), ctx.term2.getText()));
 				break;
+				
 			case SVMLexer.MULT:
-				code[i++] = SVMParser.MULT;
-				code[i++] = Integer.parseInt(ctx.res.getText());
-				code[i++] = Integer.parseInt(ctx.term1.getText());
-				code[i++] = Integer.parseInt(ctx.term2.getText());
+				code.add(new Instruction(SVMParser.MULT, ctx.res.getText(), ctx.term1.getText(), ctx.term2.getText()));
 				break;
+				
 			case SVMLexer.DIV:
-				code[i++] = SVMParser.DIV;
-				code[i++] = Integer.parseInt(ctx.res.getText());
-				code[i++] = Integer.parseInt(ctx.term1.getText());
-				code[i++] = Integer.parseInt(ctx.term2.getText());
+				code.add(new Instruction(SVMParser.DIV, ctx.res.getText(), ctx.term1.getText(), ctx.term2.getText()));
 				break;
+				
 			case SVMLexer.ADDI:
-				code[i++] = SVMParser.ADDI;
-				code[i++] = Integer.parseInt(ctx.res.getText());
-				code[i++] = Integer.parseInt(ctx.term1.getText());
-				code[i++] = Integer.parseInt(ctx.term2.getText());
+				code.add(new Instruction(SVMParser.ADDI, ctx.res.getText(), ctx.term1.getText(), ctx.term2.getText()));
 				break;
+				
 			case SVMLexer.SUBI:
-				code[i++] = SVMParser.SUBI;
-				code[i++] = Integer.parseInt(ctx.res.getText());
-				code[i++] = Integer.parseInt(ctx.term1.getText());
-				code[i++] = Integer.parseInt(ctx.term2.getText());
+				code.add(new Instruction(SVMParser.SUBI, ctx.res.getText(), ctx.term1.getText(), ctx.term2.getText()));
 				break;
+				
 			case SVMLexer.MULTI:
-				code[i++] = SVMParser.MULTI;
-				code[i++] = Integer.parseInt(ctx.res.getText());
-				code[i++] = Integer.parseInt(ctx.term1.getText());
-				code[i++] = Integer.parseInt(ctx.term2.getText());
+				code.add(new Instruction(SVMParser.MULTI, ctx.res.getText(), ctx.term1.getText(), ctx.term2.getText()));
 				break;
+				
 			case SVMLexer.DIVI:
-				code[i++] = SVMParser.DIVI;
-				code[i++] = Integer.parseInt(ctx.res.getText());
-				code[i++] = Integer.parseInt(ctx.term1.getText());
-				code[i++] = Integer.parseInt(ctx.term2.getText());
+				code.add(new Instruction(SVMParser.DIVI, ctx.res.getText(), ctx.term1.getText(), ctx.term2.getText()));
 				break;
+				
 			case SVMLexer.LI:
-				code[i++] = SVMParser.LI;
-				code[i++] = Integer.parseInt(ctx.res.getText());
-				code[i++] = Integer.parseInt(ctx.term.getText());
+				code.add(new Instruction(SVMParser.LI, ctx.res.getText(), ctx.term.getText()));
 				break;
+				
 			case SVMLexer.LB:
-				code[i++] = SVMParser.LB;
-				code[i++] = Integer.parseInt(ctx.res.getText());
-				code[i++] = Integer.parseInt(ctx.term.getText());
+				code.add(new Instruction(SVMParser.LB, ctx.res.getText(), ctx.term.getText()));
 				break;
+				
 			case SVMLexer.STOREW:
-				code[i++] = SVMParser.STOREW;
-				code[i++] = Integer.parseInt(ctx.value.getText());
-				code[i++] = Integer.parseInt(ctx.offset.getText());
+				code.add(new Instruction(SVMParser.STOREW, ctx.value.getText(), Integer.parseInt(ctx.offset.getText()) ) );
 				break;
+				
 			case SVMLexer.LOADW:
-				code[i++] = SVMParser.LOADW;
-				code[i++] = Integer.parseInt(ctx.value.getText());
-				code[i++] = Integer.parseInt(ctx.offset.getText());
+				code.add(new Instruction(SVMParser.LOADW, ctx.value.getText(), Integer.parseInt(ctx.offset.getText()) ) );
 				break;
+				
 			case SVMLexer.LABEL:
 				labelAdd.put(ctx.l.getText(),i);
 				break;
+				
 			case SVMLexer.BRANCH:
-				code[i++] = SVMParser.BRANCH;
+				code.add(new Instruction(SVMParser.BRANCH, ctx.l.getText()));
                 labelRef.put(i++,(ctx.l!=null? ctx.l.getText():null));
 				break;
+				
 			case SVMLexer.BRANCHEQ:
-				code[i++] = SVMParser.BRANCHEQ; 
+				code.add(new Instruction(SVMParser.BRANCHEQ, ctx.term1.getText(), ctx.term2.getText(), ctx.l.getText()));
                 labelRef.put(i++,(ctx.l!=null? ctx.l.getText():null));
                 break;
+                
 			case SVMLexer.BRANCHLESSEQ:
-				code[i++] = SVMParser.BRANCHLESSEQ;
-				code[i++] = Integer.parseInt(ctx.term1.getText());
-				code[i++] = Integer.parseInt(ctx.term2.getText());
+				code.add(new Instruction(SVMParser.BRANCHEQ, ctx.term1.getText(), ctx.term2.getText(), ctx.l.getText()));
                 labelRef.put(i++,(ctx.l!=null? ctx.l.getText():null));
                 break;
+                
 			case SVMLexer.JR:
-				code[i++] = SVMParser.JR;
-				code[i++] = Integer.parseInt(ctx.reg.getText());
+				code.add(new Instruction(SVMParser.JR, ctx.reg.getText()));
 				break;
+				
 			case SVMLexer.JAL:
-				code[i++] = SVMParser.JAL;
+				code.add(new Instruction(SVMParser.JAL, ctx.l.getText()));
 				labelRef.put(i++, ctx.l.getText());
 				break;
+				
 			case SVMLexer.MOVE:
-				code[i++] = SVMParser.MOVE;
-				code[i++] = Integer.parseInt(ctx.to.getText());
-				code[i++] = Integer.parseInt(ctx.from.getText());
+				code.add(new Instruction(SVMParser.MOVE, ctx.to.getText(), ctx.from.getText()));
 				break;
+				
 			case SVMLexer.PRINT:
-				code[i++] = SVMParser.PRINT;
-				code[i++] = Integer.parseInt(ctx.reg.getText());
+				code.add(new Instruction(SVMParser.PRINT, ctx.reg.getText()));
 				break;
+				
 			case SVMLexer.DELETION:
-				code[i++] = SVMParser.DELETION;
-				code[i++] = Integer.parseInt(ctx.reg.getText());
+				code.add(new Instruction(SVMParser.DELETION, ctx.reg.getText()));
 				break;
+				
 			case SVMLexer.AND:
-				code[i++] = SVMParser.AND;
-				code[i++] = Integer.parseInt(ctx.res.getText());
-				code[i++] = Integer.parseInt(ctx.term1.getText());
-				code[i++] = Integer.parseInt(ctx.term2.getText());
+				code.add(new Instruction(SVMParser.AND, ctx.res.getText(), ctx.term1.getText(), ctx.term2.getText()));
 				break;
+				
 			case SVMLexer.OR:
-				code[i++] = SVMParser.OR;
-				code[i++] = Integer.parseInt(ctx.res.getText());
-				code[i++] = Integer.parseInt(ctx.term1.getText());
-				code[i++] = Integer.parseInt(ctx.term2.getText());
+				code.add(new Instruction(SVMParser.OR, ctx.res.getText(), ctx.term1.getText(), ctx.term2.getText()));
 				break;
+				
 			case SVMLexer.NOT:
-				code[i++] = SVMParser.NOT;
-				code[i++] = Integer.parseInt(ctx.res.getText());
-				code[i++] = Integer.parseInt(ctx.term1.getText());
+				code.add(new Instruction(SVMParser.NOT, ctx.res.getText(), ctx.term1.getText()));
 				break;
+				
 			case SVMLexer.ANDB:
-				code[i++] = SVMParser.ANDB;
-				code[i++] = Integer.parseInt(ctx.res.getText());
-				code[i++] = Integer.parseInt(ctx.term1.getText());
-				code[i++] = Integer.parseInt(ctx.term2.getText());
+				code.add(new Instruction(SVMParser.ANDB, ctx.res.getText(), ctx.term1.getText(), ctx.term2.getText()));
 				break;
+				
 			case SVMLexer.ORB:
-				code[i++] = SVMParser.ORB;
-				code[i++] = Integer.parseInt(ctx.res.getText());
-				code[i++] = Integer.parseInt(ctx.term1.getText());
-				code[i++] = Integer.parseInt(ctx.term2.getText());
+				code.add(new Instruction(SVMParser.ORB, ctx.res.getText(), ctx.term1.getText(), ctx.term2.getText()));
 				break;
+				
 			case SVMLexer.NOTB:
-				code[i++] = SVMParser.NOTB;
-				code[i++] = Integer.parseInt(ctx.res.getText());
-				code[i++] = Integer.parseInt(ctx.term1.getText());
+				code.add(new Instruction(SVMParser.NOTB, ctx.res.getText(), ctx.term1.getText()));
 				break;
+				
 			case SVMLexer.HALT:
-				code[i++] = SVMParser.HALT;
-				break;             
+				code.add(new Instruction(SVMParser.HALT));
+				break;
+				
 			default:
 	            break;	// Invalid instruction
     	}
---
+++ src/util/Instruction.java
@@ -0,0 +1,77 @@
+package util;
+
+public class Instruction {
+	private int instr;
+	private String arg1;
+	private int offset;
+	private String arg2;
+	private String arg3;
+
+	public Instruction(int instr, String arg1, int offset, String arg2, String arg3) {
+		this.arg1 = arg1;
+		this.arg2 = arg2;
+		this.arg3 = arg3;
+		this.instr = instr;
+		this.offset = offset;
+	}
+	
+	public Instruction(int instr, String arg1) {
+		this.arg1 = arg1;
+		this.instr = instr;
+		this.arg2 = null;
+		this.arg3 = null;
+		this.offset = 0;
+	}
+	
+	public Instruction(int instr) {
+		this.instr = instr;		
+		this.arg1 = null;		
+		this.arg2 = null;
+		this.arg3 = null;
+		this.offset = 0;
+	}
+
+	public Instruction(int instr, String arg1, String arg2, String arg3) {
+		this.instr = instr;
+		this.arg1 = arg1;
+		this.arg2 = arg2;
+		this.arg3 = arg3;
+		this.offset = 0;
+	}
+
+	public Instruction (int instr, String arg1, String arg2) {
+		this.instr = instr;
+		this.arg1 = arg1;
+		this.arg2 = arg2;
+		this.arg3 = null;
+		this.offset = 0;
+	} 
+	
+	public Instruction(int instr, String arg1, int offset) {
+		this.instr = instr;
+		this.arg1 = arg1;
+		this.arg2 = null;
+		this.arg3 = null;
+		this.offset = offset;
+	}
+
+	public String getArg1() {
+		return this.arg1;
+	}
+
+	public String getArg2() {
+		return this.arg2;
+	}
+
+	public String getArg3() {
+		return this.arg3;
+	}
+
+	public int getInstr() {
+		return this.instr;
+	}
+
+	public int getOffset() {
+		return this.offset;
+	}
+}
