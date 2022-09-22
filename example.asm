7f0c8900999c16f302691dcf0d3526efa56966e6
carrozzogianluigi <carrozzogianluigi@gmail.com>
2022-09-22 20:46:20 +0200

    nuova versione jar

--- example.asm
+++ example.asm
@@ -4,16 +4,15 @@ push $al
 mv $fp $sp
 li $t1 0
 push $t1
-b label_0
-function_0:
-mv $fp $sp
-push $ra
 li $t1 -1
 sw $t1 0($hp)
 addi $a0 $hp 0
 addi $hp $hp 1
 push $a0
-li $a0 2
+li $t1 -1
+sw $t1 0($hp)
+addi $a0 $hp 0
+addi $hp $hp 1
 push $a0
 mv $al $fp
 addi $a0 $al -2
@@ -21,33 +20,35 @@ lw $a0 0($a0)
 lw $t1 0($sp)
 pop
 sw $t1 0($a0)
+push $fp
 mv $al $fp
+push $al
+mv $fp $sp
+li $t1 0
+push $t1
+addi $sp $sp -1
+mv $al $fp
+lw $al 0($al)
 lw $a0 -2($al)
 lw $a0 0($a0)
-li $ret 1
-b label_1
-label_1:
-addi $sp $sp 1
 push $a0
-li $a0 1
-beq $ret $a0 label_2
-lw $a0 0($sp)
+mv $al $fp
+addi $a0 $al -2
+lw $t1 0($sp)
 pop
-b label_3
-label_2:
-lw $a0 0($sp)
+sw $t1 0($a0)
+mv $al $fp
+lw $a0 -2($al)
+print $a0 1
+addi $sp $sp 1
 pop
-b function_1
-label_3:
-function_1:
-lw $ra 0($sp)
-addi $sp $sp 2
+pop
 lw $fp 0($sp)
 pop
-li $ret 0
-jr $ra
-label_0:
-addi $sp $sp 0
+mv $al $fp
+lw $a0 -2($al)
+print $a0 1
+addi $sp $sp 1
 pop
 pop
 lw $fp 0($sp)
